d8ba4b00ced7d3e0ace8cc3baeafa0ddc3f09e6c
David Jiang <djiang86@binghamton.edu>
2018-08-16 15:19:43 +0000

    bag model

---
+++ PokeLegend-Angular/src/app/shared/models/bag/bag.model.ts
@@ -0,0 +1,11 @@
+export interface Bag {
+    bagId           : number;
+    battleItemBag   : BattleItemBag;
+    berryBag        : BerryBag;
+    itemBag         : ItemBag;
+    keyItemBag      : KeyItemBag;
+    mailBag         : MailBag;
+    medicineBag     : MedicineBag;
+    pokeballBag     : PokeBallBag;
+    tmBag           : TMBag;
+}
